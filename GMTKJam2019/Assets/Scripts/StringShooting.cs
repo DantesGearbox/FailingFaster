98a45491e09f0aab93abe4aa8cc1cdf33aabe85c
DantesGearbox <mads.engberg@hotmail.com>
2019-09-27 14:42:38 +0200

    Release to detatch

--- GMTKJam2019/Assets/Scripts/StringShooting.cs
+++ GMTKJam2019/Assets/Scripts/StringShooting.cs
@@ -10,9 +10,17 @@ public class StringShooting : MonoBehaviour
 	private Vector3 stringPoint;
 	private bool stringAttached = false;
 
-	/* The force should get stronger the closer we are to the point.
-	 * Make a line renderer for the string
-	 * */
+	private float stringForceStrengthBegin = 0f;
+	private float stringForceStrengthEnd = 100f;
+
+	/* Polish
+	 * Have a line renderer from the avatar to the connection-point
+	 */
+
+	/* Design
+	 * The longer the finger has been held down, the stronger the string force is
+	 * When the finger is released, the connection is severed
+	 */
 
     // Start is called before the first frame update
     void Start()
@@ -23,20 +31,29 @@ public class StringShooting : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-		//When we click the mouse, get the direction to the mouse
+		//When we click the mouse, attach
 		if (Input.GetKeyDown(KeyCode.Mouse0)) {
+
+			//Get the direction
 			Vector3 worldMousePosition = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, transform.position.z));
 			Vector2 direction = (worldMousePosition - transform.position).normalized;
 
-			//Debug.DrawRay(transform.position, direction * 10);
+			//Get the raycast hit
+			Debug.DrawRay(transform.position, direction * 10);
 			RaycastHit2D hit2D = Physics2D.Raycast(transform.position, direction, layerMask);
 
-			//Testing
+			//Testing indented hitting
 			Vector2 point = hit2D.point;
 			point -= hit2D.normal * 0.5f;
 
+			//Attach
 			stringPoint = point;
 			stringAttached = true;
+		}
+
+		//When we let go of the screen, un-attach
+		if (Input.GetKeyUp(KeyCode.Mouse0)) {
+			stringAttached = false;
 		}
 
 		if (stringAttached) {
