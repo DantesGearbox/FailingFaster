b8112bb613fe539f7df57c42db1925f43c5893a4
DantesGearbox <mads.engberg@hotmail.com>
2019-10-24 00:12:46 +0200

    New ideas!

---
+++ GMTKJam2019/Assets/Scripts/OneFingerClassicPlatformer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OneFingerClassicPlatformer: MonoBehaviour
+{
+
+	//Design: Hold finger on screen and move away from avatar to start moving in either direction (towards finger)
+	//Quickly move finger away from avatar (out of "moving" range") and let go to dash towards the finger (ala the propulsion mechanic)
+
+	private Rigidbody2D rb;
+
+	void Start()
+	{
+
+	}
+
+	void Update()
+	{
+
+	}
+}
